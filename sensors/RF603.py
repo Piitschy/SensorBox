e30ecca502d29f0113a0ac0d393197bdbef26534
Piitschy <jan-d.pietsch@live.de>
2021-10-27 21:12:10 +0200

    Export Constants to utils

--- sensors/RF603.py
+++ sensors/RF603.py
@@ -5,11 +5,7 @@ import struct
 from typing import Tuple, List
 #import usb.core
 import serial
-from sensors import utils
-
-### DUMMY ###
-
-DATA = b'\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xd9\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xd9\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xd9\x15\x03\xda\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xdb\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xda\x15\x03\xa2s}\x00\xf4\x01\x97\x00'
+from sensors.utils import RF603 as utils
 
 ### BASE FUNKTIONS
 
@@ -54,21 +50,8 @@ class Eth(Sensor): #Default
   Returns:
       RF603.ETH: Instance of RF603 via ethernet
   """
-  NORM = int(str(4000),16)
-  #ENDIAN = '<H'
-  IP_DEST = '255.255.255.255'
-  IP_SOURCE = '192.168.0.3'
-  UDP_PORT_DEST = 603
-  UDP_PORT_SOURCE = 6003
-  STRUC = { #position in bytestring
-    'no_of_measurements': 168,
-    'len_of_measurements': 3,
-    'serial': 504,
-    'base': 506,
-    'mRange': 508
-  }
 
-  def __init__(self, ip:str=IP_SOURCE, udp_port:int=UDP_PORT_DEST, serial:int=None, ip_dest:str=IP_DEST, auto_connect=True):
+  def __init__(self, ip:str=utils.ETH.IP_SOURCE, udp_port:int=utils.ETH.UDP_PORT_DEST, serial:int=None, ip_dest:str=utils.ETH.IP_DEST, auto_connect=True):
     """Generates an Sensor object for RIFTEK RF603.
     You could find it by IP or serial number.
     Don't forget to connect after creating...
@@ -80,7 +63,7 @@ class Eth(Sensor): #Default
         ip_dest (str, optional): Target IP of the sensor (Not required). Defaults to IP_DEST.
     """
     self.ip_source = ip
-    self.sensor_addr = (self.ip_source,self.UDP_PORT_SOURCE)
+    self.sensor_addr = (self.ip_source,utils.ETH.UDP_PORT_SOURCE)
     self.ip_dest = ip_dest
     self.socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     try:
@@ -114,7 +97,7 @@ class Eth(Sensor): #Default
     
     while True:
       _, found_serial, base, mRange, addr = self.receive(filter=False)
-      if addr == (ip,self.UDP_PORT_SOURCE) or found_serial == serial:
+      if addr == (ip,utils.ETH.UDP_PORT_SOURCE) or found_serial == serial:
         print(f'Found {found_serial} on {addr[0]}:{addr[1]}')
         break
     self.ip_source = addr[0]
@@ -158,8 +141,8 @@ class Eth(Sensor): #Default
 
   def _unpack(self,data:bytes) -> Tuple[np.array, int, int, int]:
       measurements = np.array([self._struc_unpack(data,i) for i in range(0,504,3)])
-      serial =  self._struc_unpack(data,self.STRUC['serial'])
-      base = self._struc_unpack(data,self.STRUC['base'])
+      serial =  self._struc_unpack(data,utils.ETH.STRUC['serial'])
+      base = self._struc_unpack(data,utils.ETH.STRUC['base'])
       mRange = self._struc_unpack(data,self.STRUC['mRange'])
       dists = np.array([self._get_distance(m,mRange) for m in measurements])
       return dists, serial, base, mRange
@@ -200,14 +183,14 @@ class _Serial(Sensor):
 
   def request(self,cmd:str,aws:str=None):
     aws = aws or cmd
-    c:int = utils.CMDS[cmd]
-    a:dict = utils.ANS[aws]
+    c:int = utils.SERIAL.CMDS[cmd]
+    a:dict = utils.SERIAL.ANS[aws]
     r = self.write(self._cmd(c))
     return { k:self._answer(r,*v) for k,v in a.items()}
 
   def turn(self,state:str='on'): # on | off
-    c = utils.CMDS['write']
-    t = utils.PARAMS['turn']
+    c = utils.SERIAL.CMDS['write']
+    t = utils.SERIAL.PARAMS['turn']
     if state not in t:
       print('unknown state')
       return
--- sensors/utils.py
+++ sensors/utils.py
@@ -1,41 +1,44 @@
 
+class RF603:
+  class ETH:
+    NORM = int(str(4000),16)
+    ENDIAN = '<H'
+    IP_DEST = '255.255.255.255'
+    IP_SOURCE = '192.168.0.3'
+    UDP_PORT_DEST = 603
+    UDP_PORT_SOURCE = 6003
+    STRUC = { #position in bytestring
+      'no_of_measurements': 168,
+      'len_of_measurements': 3,
+      'serial': 504,
+      'base': 506,
+      'mRange': 508
+    }
 
-NORM = int(str(4000),16)
-ENDIAN = '<H'
-IP_DEST = '255.255.255.255'
-IP_SOURCE = '192.168.0.3'
-UDP_PORT_DEST = 603
-UDP_PORT_SOURCE = 6003
-STRUC = { #position in bytestring
-  'no_of_measurements': 168,
-  'len_of_measurements': 3,
-  'serial': 504,
-  'base': 506,
-  'mRange': 508
-}
 
-CMDS = {
-  'ident': [0x81],
-  'write': [0x83],
-  'measure':[0x86]
-}
+  class SERIAL:
+    CMDS = {
+      'ident': [0x81],
+      'write': [0x83],
+      'measure':[0x86]
+    }
 
-PARAMS = {
-  'turn' : {
-    'on' : [0x80,0x81],
-    'off': [0x80,0x80]
-  }
-}
+    PARAMS = {
+      'turn' : {
+        'on' : [0x80,0x81],
+        'off': [0x80,0x80]
+      }
+    }
 
-ANS = {
-  'ident': {
-    'type': [0],
-    'firmware':[1],
-    'serial':[2,2],
-    'base': [4,2],
-    'mRange':[6,2]
-  },
-  'measure':{
-    'value':[0,2]
-  }
-}
+    ANS = {
+      'ident': {
+        'type': [0],
+        'firmware':[1],
+        'serial':[2,2],
+        'base': [4,2],
+        'mRange':[6,2]
+      },
+      'measure':{
+        'value':[0,2]
+      }
+    }
