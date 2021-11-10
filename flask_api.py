8b1f94eabd751917a316784531fe0aa01b6ef00a
Jan <55839828+Piitschy@users.noreply.github.com>
2021-11-10 17:15:27 +0100

    made more routes

--- flask_api.py
+++ flask_api.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import sensors.RF603.driver as RF603
 import subprocess
 from time import sleep
-import os
+import os, json5
 import os.path as path
 from dotenv import load_dotenv
+import importlib.util
 
 ### CONST ###
 
 GIT_PATH = path.abspath(path.join(path.dirname(path.realpath(__file__)),''))
 GIT_DIR = GIT_PATH.split(path.sep)[-1]
 
+DRIVER_LOCATIONS_PATH = './utils/driver_locations.json5'
 
 ### ENV ###
 
@@ -40,6 +42,38 @@ def exec_command(name:str, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text:
     return None
   return subprocess.run(CMD[name], stdout=stdout, stderr=stderr, text=text, **kwargs).stdout
 
+def get_kwargs_from_request(request,*args) -> dict:
+  kwargs = {}
+  for arg in args:
+    if arg in request.args:
+      kwargs.update({arg:request.args[arg]})
+    else:
+      kwargs.update({arg:None})
+  return kwargs
+
+def get_driver_list(filter_list:list=None):
+  with open(DRIVER_LOCATIONS_PATH) as f:
+    sources = json5.load(f)
+  if not filter_list:
+    return sources
+  return {k:v for k, v in sources.items() if k in filter_list}
+
+def load_sensors(mode:str, devices:list=None):
+  def import_from_path(name:str,path:str):
+    spec = importlib.util.spec_from_file_location(name, path)
+    driver = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(driver)
+    return driver
+
+  def multi_import(sources:dict,mode:str=mode):
+    drivers = [import_from_path(source['name'],source['path']) for source in sources]
+    return [getattr(driver, mode) for driver in drivers]
+  
+  sources = get_driver_list(devices)
+  drivers = multi_import(sources, mode)
+  sensors = [driver() for driver in drivers]
+  return sensors
+
 ### FLASK ###
 
 app = Flask(__name__)
@@ -59,12 +93,22 @@ def update():
 def reboot():
   return exec_command('reboot')
 
-@app.route('/measure', methods=['GET'])
-def measure():
-  s = RF603.Serial()
-  m = s.measure()
-  print(m)
-  return str(m)
+@app.route('/sensores/add', methods=['GET'])
+def detect_sensors():
+  kwargs = get_kwargs_from_request(request,"device")
+  return 
+
+@app.route('/sensores/drivers', methods=['GET'])
+def show_drivers():
+  kwargs = get_kwargs_from_request(request,"device")
+  drivers = get_driver_list(kwargs["device"])
+  return drivers
+
+@app.route('/sensores/measure', methods=['GET'])
+def get_measure():
+  ms = {s.device:s.measure() for s in sensors}
+  return ms
 
 if __name__ =='__main__':
-    app.run(host="0.0.0.0", debug=True)
+  
+  app.run(host="0.0.0.0", debug=True)
