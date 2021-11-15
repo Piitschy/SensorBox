7c2c6a222303b15106f9a7f088d15d8e036d7f1c
Jan <55839828+Piitschy@users.noreply.github.com>
2021-11-15 21:19:09 +0100

    create Settings

---
+++ sensor-gui/src/views/Settings.vue
@@ -0,0 +1,17 @@
+<template>
+  <hello-world />
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Settings from '../components/Settings.vue'
+
+  export default Vue.extend({
+    name: 'Settings',
+
+    components: {
+      Settings,
+    },
+
+  })
+</script>
