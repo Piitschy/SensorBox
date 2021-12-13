b6d45b0d08c44eedfaa2c6da93bf382359002a62
Jan <55839828+Piitschy@users.noreply.github.com>
2021-12-13 18:13:18 +0100

    add dialog

--- sensor-gui/src/views/Messungen.vue
+++ sensor-gui/src/views/Messungen.vue
@@ -1,33 +1,50 @@
 <template>
-<div>
-  <v-overlay :value="selectedItem">
-    <v-card width="100%" height="600px" class="pa-4">
-      {{selectedItem}}
+  <div>
+    <v-dialog
+        transition="dialog-top-transition"
+        max-width="600"
+        :value="dialog"
+        v-if="dialog"
+      >
+        <template>
+          <v-card>
+            <v-toolbar>
+               <p>{{selectedItem.name}}</p>
+               <p class="text-right">{{selectedItem.start_date}}</p>
+            </v-toolbar>
+            {{measurements}}
+            <v-card-actions class="justify-end">
+              <v-btn
+                text
+                @click="dialog = false"
+              >Schließen</v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+    <v-card>
+      
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Suche nach Messdaten"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="measurements"
+        :items-per-page="15"
+        :search="search"
+        :loading="loading"
+        class="elevation-1"
+        @click:row="click_on_row"
+      ></v-data-table>
+      <!--<Messung />-->
     </v-card>
-  </v-overlay>
-  <v-card>
-    {{measurements}}
-    <v-card-title>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Suche nach Messdaten"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="measurements"
-      :items-per-page="15"
-      :search="search"
-      :loading="loading"
-      class="elevation-1"
-      @click:row="click_on_row"
-    ></v-data-table>
-    <!--<Messung />-->
-  </v-card>
-</div>
+  </div>
 </template>
 
 <script>
@@ -44,6 +61,7 @@
       return {
         search: '',
         loading: false,
+        dialog: false,
         selectedItem: null,
         headers: [
           {text: 'Namen', value: 'name'},
@@ -71,13 +89,13 @@
         return result
       },
       click_on_row(item) {
-        this.selectedItem = item.id
+        this.selectedItem = item
+        this.dialog = true
       }
     },
 
     async mounted() {
       this. measurements = await this.getData()
     }
-
   })
 </script>
